53d298c694d157f03b271a90bb20e40e15d4e208
SeugnshinLee <lsver67@gmail.com>
2022-05-19 16:54:14 +0900

    로드셀 보정 프로그램 개발

---
+++ Loadcell/Loadcell_ManualCali.ino
@@ -0,0 +1,61 @@
+/* 5선식 Load cell 작동 코드 */
+
+/* ============ 핀 레이아웃 ============ */
+/* HX711 - Loadcell
+ * E+(VCC) -> 빨간색선
+ * E-(GND) -> 검은색선
+ * O+(OUTPUT) -> 흰색선
+ * O-  ->  초록색선
+ * 
+ * 노란색선은 EMI 노이즈 제거용, 사용 안해도 무방
+ */
+
+#include "HX711.h"
+
+// HX711 - DOUT, DT (데이터선) 
+// HX711 - CLK, SCK (클럭선) 
+const int DOUT = 2;
+const int CLK = 3;
+
+HX711 scale;
+
+float calibration_factor = 21000;    //로드셀 종류나 상황에 따라 적당한 값으로 시작
+
+void setup() {
+  Serial.begin(9600);
+  scale.begin(DOUT,CLK);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale...");
+  delay(5000);
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + to increase calibration factor");
+  Serial.println("Press - to decrease calibration factor");
+
+  scale.set_scale();
+  scale.tare();  //Reset the scale to 0
+
+  long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
+}
+
+void loop() {
+
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units());
+  Serial.print(" kg");
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+
+  if(Serial.available())
+  {
+    char key = Serial.read();
+    if(key == '+')
+      calibration_factor += 10;
+    else if(key == '-')
+      calibration_factor -= 10;
+  }
+}
