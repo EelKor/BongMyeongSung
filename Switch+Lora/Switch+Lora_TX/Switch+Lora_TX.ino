d861f2f5f04737f48e83bbf1e2fb1da82f5a85c0
SeugnshinLee <lsver67@gmail.com>
2022-05-26 09:17:46 +0900

    Lora 모듈을 이용한 원격점화장치 초안

---
+++ Switch+Lora/Switch+Lora_TX/Switch+Lora_TX.ino
@@ -0,0 +1,47 @@
+#include <SoftwareSerial.h>
+
+#define SWITCH 2
+
+unsigned long lastTransmission;
+const int interval = 1000;
+int ignition = 1;
+SoftwareSerial lora(2,3);
+
+void setup(){
+    Serial.begin(115200);
+
+    // Lora 모듈 초기화
+    lora.begin(9600);
+    delay(100);
+    lora.println("AT+PARAMETER=10,7,1,7");
+    delay(100);
+    lora.println("AT+ADDRESS=76");
+    delay(100);
+    lora.println("AT+NETWORKID=2");
+    delay(100);
+    //lora.println("AT+BAND=92000000");
+    delay(100);
+
+    // 핀 선언
+    pinMode(pinSwitch,INPUT_PULLUP);
+    pinMode(LED_BUILTIN,OUTPUT);
+}
+
+void loop()
+{     
+  String cmd = "AT+SEND=77,1,";
+  ignition = digitalRead(pinSwitch);
+
+  if(ignition == HIGH)
+  {
+    digitalWrite(LED_BUILTIN,HIGH);
+    lora.println(cmd+String(ignition));
+  }
+
+  while(lora.available())
+  {
+    Serial.write(lora.read());
+  }
+  delay(100);
+
+}
